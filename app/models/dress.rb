2ae262b374f9efc226174767d700b815b18beb1f
kawakubox <shigefumi.kawakubo@gmail.com>
2019-10-07 07:20:11 +0900

    Change column type to varchar

--- app/models/dress.rb
+++ app/models/dress.rb
@@ -3,8 +3,6 @@
 class Dress < ApplicationRecord
   extend ActiveHash::Associations::ActiveRecordExtensions
 
-  enum rarity: { ultra_rare: 4, super_rare: 3, rare: 2, normal: 1 }
-
   belongs_to :heroine
   has_many :dress_parameters, dependent: :delete_all
 end
