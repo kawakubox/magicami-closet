964b5d12b3395875260fe17ffdf271ae61e18ce0
kawakubox <shigefumi.kawakubo@gmail.com>
2019-10-08 08:59:46 +0900

    Update db/seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -15,14 +15,26 @@ HEADER_DICT = {
   '防御': :defense,
   '速度': :agility,
   '的中': :dexterity,
-  '抵抗': :resist
-}.freeze.with_indifferent_access
+  '抵抗': :resist,
+  'スキル名_1': :skill_name_1,
+  'スキル最大レベル_1': :skill_max_level_1,
+  'スキルリキャスト_1': :skill_recast_1,
+  'スキル説明_1': :skill_description_1,
+  'スキル名_2': :skill_name_2,
+  'スキル最大レベル_2': :skill_max_level_2,
+  'スキルリキャスト_2': :skill_recast_2,
+  'スキル説明_2': :skill_description_2,
+  'スキル名_3': :skill_name_3,
+  'スキル最大レベル_3': :skill_max_level_3,
+  'スキルリキャスト_3': :skill_recast_3,
+  'スキル説明_3': :skill_description_3
+}.with_indifferent_access
 
 def reset_tables
+  Dress.destroy_all
   con = ActiveRecord::Base.connection
-  con.execute('DELETE FROM dress_parameters')
   con.execute("SELECT setval ('dress_parameters_id_seq', 1, false)")
-  con.execute('DELETE FROM dresses')
+  con.execute("SELECT setval ('skills_id_seq', 1, false)")
   con.execute("SELECT setval ('dresses_id_seq', 1, false)")
 end
 
@@ -48,6 +60,15 @@ def dress_parameter_param(row)
   }
 end
 
+def skill_param(row, index)
+  {
+    number: index,
+    name: row["skill_name_#{index}".to_sym],
+    max_level: row["skill_max_level_#{index}".to_sym],
+    description: row["skill_description_#{index}".to_sym],
+  }
+end
+
 reset_tables
 
 csv = CSV.read('db/fixtures/dresses.tsv',
@@ -58,4 +79,7 @@ csv = CSV.read('db/fixtures/dresses.tsv',
 csv.each do |row|
   dress = Dress.create!(dress_param(row))
   DressParameter.create!(dress: dress, **dress_parameter_param(row))
+  (1..3).each do |i|
+    Skill.create!(dress: dress, **skill_param(row, i)) if row["skill_name_#{i}".to_sym].present?
+  end
 end
