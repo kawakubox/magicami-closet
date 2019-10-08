7f40b0dd40c196633712829994d4a1608bdbbd10
kawakubox <shigefumi.kawakubo@gmail.com>
2019-10-09 08:48:57 +0900

    Leak a commit

--- spec/factories/skills.rb
+++ spec/factories/skills.rb
@@ -7,5 +7,12 @@ FactoryBot.define do
     name { Faker::Games::Pokemon.move }
     max_level { Random.rand(1..9) }
     description { Faker::Lorem.sentence }
+
+    after(:build) do |skill|
+      skill.skill_variables << [
+        build(:skill_variable, skill: skill, level: 1),
+        build(:skill_variable, skill: skill, level: 2)
+      ]
+    end
   end
 end
